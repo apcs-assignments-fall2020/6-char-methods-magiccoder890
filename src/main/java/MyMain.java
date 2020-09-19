0bf3d250b472cb1fe98d3928c1e704b019feadfc
magiccoder890 <23fzhu@athenian.org>
2020-09-19 14:22:17 +0000

    This is Bert's new homework

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,39 +2,76 @@ import java.util.Scanner;
 
 public class MyMain {
 
-    // Changes a capital letter to a lowercase letter
-    // If the char is not an uppercase letter, 
-    // it is returned unchanged. 
     public static char toLower(char ch) {
         // REPLACE WITH YOUR CODE
-        return '!';
+        int num_var = ch;
+        if (num_var >= 65 && num_var <= 90) {
+            num_var += 32;
+            ch = (char) num_var;
+            System.out.print(ch);
+        } 
+        else if (num_var < 65 || num_var > 90) {
+            ch = (char) num_var;
+            System.out.print(ch);
+        }
+        return ch;
     }
 
-    // Returns a copy of a String, where all letters 
-    // are now lowercase
-    public static String myToLowerCase(String str) {
-        // REPLACE WITH YOUR CODE
-        return "";
-    }
-
-    // Changes a lowercase letter to an uppercase letter
-    // If the char is not an lowercase letter, 
-    // it is returned unchanged. 
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
+        int num_var = ch;
+        if (num_var >= 97 && num_var <= 122) {
+            num_var -= 32;
+            ch = (char) num_var;
+            System.out.print(ch);
+        } 
+        else if (num_var < 97 || num_var > 122) {
+            ch = (char) num_var;
+            System.out.print(ch);
+        } 
+        return ch;
     }
 
-    // Returns a copy of a String, where all letters 
-    // are now uppercase
     public static String myToUpperCase(String str) {
-        // REPLACE WITH YOUR CODE HERE
-        return "";
+        String new_sentence = "";
+        for (int i = 0; i < str.length(); i++) {
+            char every_word = str.charAt(i);
+            new_sentence += toUpper(every_word);
+        }
+        return new_sentence;
     }
 
+    public static String myToLowerCase(String str) {
+        String new_sentence = "";
+        for (int i = 0; i < str.length(); i++) {
+            char every_word = str.charAt(i);
+            new_sentence += toLower(every_word);
+        }
+        return new_sentence;
+    }
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please input what service you want");
+        String chioce = scanner.next();
+        if ("toLower".equals(chioce)) {
+            System.out.print("Please input a char");
+            char ch = scanner.next().charAt(0);
+            toLower(ch);
+        }
+        if ("toUpper".equals(chioce)) {
+            System.out.print("Please input a char");
+            char ch = scanner.next().charAt(0);
+            toUpper(ch);
+        }
+        if ("myToLowerCase".equals(chioce)) {
+            System.out.print("Please input a string");
+            String str = scanner.nextLine();
+            myToLowerCase(str);
+        }
+        if ("myToUpperCase".equals(chioce)) {
+            System.out.print("Please input a string");
+            String str = scanner.nextLine();
+            myToUpperCase(str);
+        }
     }
 }
